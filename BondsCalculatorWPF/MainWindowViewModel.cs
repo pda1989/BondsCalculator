62804b1fdd5bb9fa0fb0bb3a2bf1c89880dac70c
Dmitry Pokhlebaev <pokhlebaev.dmitry@gmail.com>
2016-09-11 17:26:00 +0800

    Add data validation

--- BondsCalculatorWPF/MainWindowViewModel.cs
+++ BondsCalculatorWPF/MainWindowViewModel.cs
@@ -29,42 +29,84 @@ namespace BondsCalculatorWPF
         public double Nominal
         {
             get { return _nominal; }
-            set { SetProperty(ref _nominal, value); }
+            set
+            {
+                if (value > 0)
+                    SetProperty(ref _nominal, value);
+            }
         }
         public double Coupon
         {
             get { return _coupon; }
-            set { SetProperty(ref _coupon, value); }
+            set
+            {
+                if (value > 0)
+                    SetProperty(ref _coupon, value);
+            }
         }
         public double ACI
         {
             get { return _aci; }
-            set { SetProperty(ref _aci, value); }
+            set
+            {
+                if (value > 0)
+                    SetProperty(ref _aci, value);
+            }
         }
         public double Redemption
         {
             get { return _redemption; }
-            set { SetProperty(ref _redemption, value); }
+            set
+            {
+                if (value >= 1)
+                    SetProperty(ref _redemption, value);
+            }
         }
         public double MaxPrice
         {
             get { return _maxPrice; }
-            set { SetProperty(ref _maxPrice, value); }
+            set
+            {
+                if (value > 0)
+                    SetProperty(ref _maxPrice, value);
+                if (value < _minPrice)
+                {
+                    _minPrice = value;
+                    OnPropertyChanged("MinPrice");
+                }
+            }
         }
         public double MinPrice
         {
             get { return _minPrice; }
-            set { SetProperty(ref _minPrice, value); }
+            set
+            {
+                if (value > 0)
+                    SetProperty(ref _minPrice, value);
+                if (value > _maxPrice)
+                {
+                    _maxPrice = value;
+                    OnPropertyChanged("MaxPrice");
+                }
+            }
         }
         public double PriceStep 
         {
             get { return _priceStep; }
-            set { SetProperty(ref _priceStep, value); }
+            set
+            {
+                if (value > 0)
+                    SetProperty(ref _priceStep, value);
+            }
         }
         public int Days
         {
             get { return _days; }
-            set { SetProperty(ref _days, value); }
+            set
+            {
+                if (value >= 365 && value <= 366)
+                    SetProperty(ref _days, value);
+            }
         }
         public ObservableCollection<ResultRow> Results { get; set; } = new ObservableCollection<ResultRow>();
 
@@ -78,6 +120,7 @@ namespace BondsCalculatorWPF
             MaxPrice = 100;
             PriceStep = 0.1;
             Days = 365;
+            Redemption = 1;
 
             CalculateCommand = new DelegateCommand(CalculateProfitability);
         }
@@ -90,17 +133,18 @@ namespace BondsCalculatorWPF
                 var price = Math.Round(i, 2);
                 var buy = _nominal * price / 100 + _aci;
                 var sell = _nominal + _coupon;
+                var profit = Math.Round(sell - buy, 2);
                 var profitability = 0d;
                 try
                 {
-                    profitability = Math.Round((sell - buy) / buy * 100 / _redemption * _days, 2);
+                    profitability = Math.Round(profit / buy * 100 / _redemption * _days, 2);
                 }
                 catch (Exception) { }
                 Results.Add(new ResultRow
                 {
                     Buy = buy,
                     Price = price,
-                    Profit = sell - buy,
+                    Profit = profit,
                     Sell = sell,
                     Profitability = profitability
                 });
